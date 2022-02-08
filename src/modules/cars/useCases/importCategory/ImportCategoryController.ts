4d1d45897d1bfa2b4d601dd44f3f797472efa23c
Gian Pierre <gianpierresb@gmail.com>
2022-02-08 17:01:19 -0300

    First Commit

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -0,0 +1,19 @@
+import {Request, Response} from "express"
+import {ImportCategoryUseCase} from "./ImportCategoryUseCase"
+import {container} from "tsyringe"
+
+class ImportCategoryController{
+    
+    async handle(request: Request, response: Response): Promise<Response>{
+
+        const{file} = request
+
+        const importCategoryUseCase = container.resolve(ImportCategoryUseCase)
+
+        importCategoryUseCase.execute(file)
+
+        return response.sendStatus(200)
+    }
+}
+
+export {ImportCategoryController}
