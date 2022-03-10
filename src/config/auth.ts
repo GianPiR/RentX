dd17fae322a5a4b094a94837dadf568939e2f4d5
Gian Pierre <gianpierresb@gmail.com>
2022-03-10 14:15:10 -0300

    Fix Refresh Token

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
     secret_token: "18160eae74ccbf2be2d31a0422f64c0e",
-    expires_in_token: "15m",
+    expires_in_token: "30s",
     secret_refresh_token: "987cd631041b5b6ea4698bfde9798cfd",
     expires_in_refresh_token: "30d",
     expires_refresh_token_days: 30,
--- src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -10,6 +10,11 @@ interface IPayload {
     email: string;
 }
 
+interface ITokenResponse {
+    token: string;
+    refresh_token: string;
+}
+
 @injectable()
 class RefreshTokenUseCase {
     constructor(
@@ -19,7 +24,7 @@ class RefreshTokenUseCase {
         private dateProvider: IDateProvider
     ) {}
 
-    async execute(token: string) {
+    async execute(token: string): Promise<ITokenResponse> {
         const { email, sub } = verify(token, auth.secret_refresh_token) as IPayload
         
         const user_id = sub
@@ -46,8 +51,13 @@ class RefreshTokenUseCase {
             refresh_token,
             user_id
         })
+        
+        const newToken = sign({}, auth.secret_token, {
+            subject: user_id,
+            expiresIn: auth.expires_in_token
+        })
 
-        return refresh_token
+        return { refresh_token, token: newToken} 
     }
 }
 
